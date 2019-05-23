81a0a308bf94ed2fe255c527611243722e561572
Jason Williams <JasonJWilliamsNY@gmail.com>
2019-05-22 20:04:41 -0400

    script accepts tab-delimited spreadsheets

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # lesson-templates
 Templates and tools for creating GEA hosted lessons and documentation
+
+## Going from google form to restructured text
+
+1. Lesson submission will use a form like a google form to enforce controlled
+ vocabulary.
+2. A single row in the resulting spreadsheet will be a submission. This must be
+copied into a sheet with safe column headers (TODO: automate). This two row
+spreadsheet is saved as a tab-delimited file.
+3. jinja-render.py is called to push the data from the spreesheet into a
+restructured text template.
+  `python jinja-render.py -d ~/path/to/template -t template.rst -c data.tsv -o rendered.rst`
--- jinja-render.py
+++ jinja-render.py
@@ -6,17 +6,6 @@ from jinja2 import Environment, FileSystemLoader
 
 def main():
     
-    '''
-    def excelcsvtopipe(csv_file):
-        """Converts an Excel comma-delimited csv file to pipe-delimited"""
-        with open(csv_file, "rt", encoding="utf8") as file:
-            reader = csv.reader(file)
-            for rows in reader:
-                writer=csv.writer(open('temp.csv','w'),delimiter='|')
-                writer.writerow(rows)
-      '''           
-    
-    
     def gettemplate(template_dir, template_name):
         """get the template location and name to load into the Jinja functions"""
         file_loader = FileSystemLoader(template_dir) 
@@ -26,31 +15,34 @@ def main():
     
     #open the csv file and print template
     
-    def renderfromcsv(csv_file,outfile,gettemplate):
+    def renderfromcsv(tab_delimited_file,outfile,gettemplate):
         """ Imports a CSV file as a dictionary - headers are keys; print into the jinja template"""
-        with open(csv_file, "rt", encoding="utf-8-sig") as file:
-            reader=csv.DictReader(file)
+        # indicate a new dialect with \t as the delimiter
+        csv.register_dialect("tab-delimited", delimiter="\t")
+        # use the utf-8-sug to get rid of pesky byte encoding characters 
+        with open(tab_delimited_file, "rt", encoding="utf-8-sig") as file:
+            # read data into a dictionary
+            reader=csv.DictReader(file,dialect="tab-delimited")
             for values in reader:
-                print(values)
                 gen_template = gettemplate(template_dir, template_name)
                 output = gen_template.render(values)
+                # substitue into jinja template
             with open(outfile, 'w') as outfile:
                 print(output, file=outfile)   
     
-    #excelcsvtopipe(csv_file)
-    renderfromcsv(csv_file,outfile,gettemplate)
+
+    renderfromcsv(tab_delimited_file,outfile,gettemplate)
 
 if __name__ == '__main__':
-    parser=argparse.ArgumentParser(description="Move data from a CSV file into a ReStructured Text Jinja2 Template")
+    parser=argparse.ArgumentParser(description="Move data from a tab delimited file into a ReStructured Text Jinja2 Template")
     parser.add_argument('--template-dir', '-d' , dest="template_dir", type=str, nargs=1, help="directory path where template file is located", required=True)
     parser.add_argument('--template-name', '-t' , dest="template_name", type=str, nargs=1, help="name of template file", required=True)
-    parser.add_argument('--csv-file', '-c', dest="csv_file", type=str, nargs=1, help="path to the csv file", required=True)
+    parser.add_argument('--tab-delimited-file', '-c', dest="tab_delimited_file", type=str, nargs=1, help="path to the tab delimited file", required=True)
     parser.add_argument('--outfile', '-o', dest="outfile",type=str, nargs=1, help="name and path for output file", required=True)
     args = parser.parse_args()    
     template_dir=args.template_dir[0]
     template_name=args.template_name[0]
-    csv_file=args.csv_file[0]
+    tab_delimited_file=args.tab_delimited_file[0]
     outfile=args.outfile[0]
-    #tempfile='temp.csv'
     main()
 
